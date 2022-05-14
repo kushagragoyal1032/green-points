3f0897dd76c6e2bdaa339d515c468b46ef898823
ketan <ketangoyal1042@gmail.com>
2022-05-14 10:32:14 +0530

    Final commit after align css and fixed major bugs

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <!-- <script src="https://cdn.tailwindcss.com"></script> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script> -->
     <script src="https://kit.fontawesome.com/742d0b1255.js" crossorigin="anonymous"></script>
     <title>GreenPoints | Home Users  Welcome</title>
 </head>
@@ -115,7 +115,7 @@
     <!--    services  -->
     <section class="services bg-light my-2">
         <div class="container text-center">
-            <h1 class="title">WHAT WE ACTUALLY DO</h1>
+            <h1 class="title ">WHAT WE ACTUALLY DO</h1>
             <div class="row">
                 <div class="col-md-4 services">
                     <img src="images/social-media.gif" alt="">
@@ -223,7 +223,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
     const alerts = document.getElementsByClassName("alert");
 
